bc7902d469d857c72c24454ad72daf6b8e33cc49
Dylan Nelson <dylanonelson@dropbox.com>
2020-02-07 19:47:20 -0500

    Configure typescript & webpack

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import './index.html';
+import './styles.css';
+import * as ComponentManager from 'sn-components-api';
+
+function init() {
+  const componentManager = new ComponentManager();
+  console.log(componentManager);
+}
+
+init();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
+const path = require('path');
+
 module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    index: './index.html',
+    port: 1104,
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.ts$/i,
+        use: ['ts-loader'],
+        exclude: '/node_modules/',
+      },
+      {
+        exclude: '/node_modules/',
+        options: {
+          name: '[name].[ext]',
+        },
+        test: /\.html$/i,
+        loader: 'file-loader',
+      },
     ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
